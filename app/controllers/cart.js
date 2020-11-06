7000db7215b8e9c03fd80778f2201eca131097c0
Naman Jain <namandude1008@gmail.com>
2020-11-06 16:37:02 +0530

    added controller

---
+++ app/controllers/cart.js
@@ -0,0 +1,6 @@
+import Controller from '@ember/controller';
+
+export default class CartController extends Controller {//anythig defined in this class can be used in template
+    cartName = "MyFirstCart";
+    cartSubTotal = 0;
+}
--- app/routes/cart.js
+++ app/routes/cart.js
@@ -1,4 +1,25 @@
 import Route from '@ember/routing/route';
 
 export default class CartRoute extends Route {
+    model(){
+        const items = [
+            {
+                name : "Item1",
+                price : 100
+            },
+            {
+                name : "Item2",
+                price : 400
+            }
+        ];
+        return items;
+    }
+    setupController(controller, model){
+       super.setupController(controller,model);
+        let subtotal = 0;
+        model.forEach(element => {
+            subtotal = subtotal+element.price;
+        });
+        controller.set('cartSubTotal' , subtotal);
+    }
 }
